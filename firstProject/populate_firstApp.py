86c1e459085df821b4261d4aac7b389f4bff916b
Harish Lal <harishlal80@gmail.com>
2020-11-13 00:12:57 +0530

    update

--- firstProject/populate_firstApp.py
+++ firstProject/populate_firstApp.py
@@ -5,34 +5,33 @@ import django
 django.setup()
 
 import random
-from firstApp.models import AccessRecord,Webpage,Topic
 from faker import Faker
+try: 
+    fakegen = Faker()
+    topics =['Search','Social','Marketplace','News','Games']
 
-fakegen = Faker()
-topics =['Search','Social','Marketplace','News','Games']
+    def addTopic():
+        t = Topic.objects.get_or_create(top_name = random.choice(topics))[0]
+        t.save()
+        return t
 
-def addTopic():
-    t = Topic.objects.get_or_create(top_name = random.choice(topics))[0]
-    t.save()
-    return t
+    def populate(N=5):
+        for entry in range(N):
+            # get the topic for the entry
+            top = addTopic()
 
-def populate(N=5):
-    for entry in range(N):
-        # get the topic for the entry
-        top = addTopic()
+            # create the fake data for the entry
+            fake_url = fakegen.url()
+            fake_name = fakegen.company()
 
-        # create the fake data for the entry
-        fake_url = fakegen.url()
-        fake_data = fakegen.data()
-        fake_name = fakegen.company()
+            #create new webpage entry
+            webpge =  Webpage.objects.get_or_create(topic=top,url=fake_url,name=fake_name)[0]
 
-        #create new webpage entry
-        webpge =  Webpage.objects.get_or_create(topic=top,url=fake_url,name=fake_name)[0]
+    if __name__ == "__main__":
+        print("populating script")
+        populate(20)
+        print("populating complete")
 
-        #create fake access record for that webpage
-        accRec = AccessRecord.objects.get_or_create(name=webpge,data=fake_data)[0]
-
-if __name__ == "__main__":
-    print("populating script")
-    populate(20)
-    print("populating complete")
+except:
+    AttributeError
+    print("Error Occuring")
