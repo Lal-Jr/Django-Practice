cf2152734d1f48a3ba3c5fc59fe548ac648f576a
Harish Lal <harishlal80@gmail.com>
2020-11-13 22:42:27 +0530

    1

--- firstProject/firstApp/views.py
+++ firstProject/firstApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse    
+from django.http import HttpResponse
+from firstApp.models import Topic,AccessRecords,Webpage    
 
 def index(request):
+    webpages_list = AccessRecords.objects.order_by('date')
     myDict = {'insert_me':'HARISH'}
     return render(request,'firstApp/index.html',context=myDict)
